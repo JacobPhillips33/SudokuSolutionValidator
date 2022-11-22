47335db97050169acfed0f2bf52cc0cc552fa16d
Jacob Phillips <jakephillips.wv@gmail.com>
2022-11-22 10:52:27 -0600

    adjusted formatting

--- SudokuSolutionValidator/Program.cs
+++ SudokuSolutionValidator/Program.cs
@@ -20,23 +20,35 @@ int[][] board = new int[9][];
 board[0] = new int[9];
 
 Console.WriteLine("Enter each row of numbers separated by a \",\"");
-Console.WriteLine("First row: ");
-board[0] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
-Console.WriteLine("Second row: ");
-board[1] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
-Console.WriteLine("Third row: ");
-board[2] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
-Console.WriteLine("Forth row: ");
-board[3] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
-Console.WriteLine("Fifth row: ");
-board[4] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
-Console.WriteLine("Sixth row: ");
-board[5] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
-Console.WriteLine("Seventh row: ");
-board[6] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
-Console.WriteLine("Eigth row: ");
-board[7] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
-Console.WriteLine("Ninth row: ");
-board[8] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
+Console.WriteLine();
 
+Console.Write("First row:   ");
+board[0] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
+
+Console.Write("Second row:  ");
+board[1] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
+
+Console.Write("Third row:   ");
+board[2] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
+
+Console.Write("Fourth row:  ");
+board[3] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
+
+Console.Write("Fifth row:   ");
+board[4] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
+
+Console.Write("Sixth row:   ");
+board[5] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
+
+Console.Write("Seventh row: ");
+board[6] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
+
+Console.Write("Eigth row:   ");
+board[7] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
+
+Console.Write("Ninth row:   ");
+board[8] = Console.ReadLine().Split(",").Select(x => Int32.Parse(x)).ToArray();
+Console.WriteLine();
+
+Console.Write("Valid Sudoku Solution: ");
 Console.WriteLine(Methods.ValidateSolution(board));
